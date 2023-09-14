80a2ec53ab97d74ae8ce19344380907a4feafb24
Olga Pecenkova <olga.pecenkova@gmail.com>
2023-09-14 21:40:14 +0200

    index_ukoly.php - formulář zadávání úkolů

--- index_ukoly.php
+++ index_ukoly.php
@@ -15,7 +15,18 @@
   <form action="logout.php" method="post">
       <button type="submit">Odhlásit se</button>
   </form>
-
+  <form class="tasks-container" action="" method="POST">
+    <input type="text" id="task-input" name="task-input" placeholder="Zadejte úkol ...">
+    <input type="text" id="task-input" name="task-podrobnosti" placeholder="Zadejte podrobnosti ...">
+    <input type="date" id="task-input-pul" name="task-termin">
+    <select id="task-input-pul" name="task-priorita">
+      <option value="" disabled selected>Priorita...</option>
+      <option value="Nízká">Nízká</option>
+      <option value="Střední">Střední</option>
+      <option value="Vysoká">Vysoká</option>
+    </select>
+    <button type="submit">Přidat&nbsp;úkol</button>
+  </form>
 
 
   </body>
