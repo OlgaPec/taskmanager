32dc8bf5f7695c8b25bfb64c5231b06d0f752dc1
Olga Pecenkova <olga.pecenkova@gmail.com>
2023-09-14 20:31:08 +0200

    v souboru index.php opraveno zdvojení výpisu

--- index.php
+++ index.php
@@ -19,26 +19,7 @@ index.php
  
   </div>
   
-  index.php
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Task manager</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <script src="script.js"></script>
-</head>
-<body>
-
-<h1>Task manager</h1>
   
-  <div class="buttons-container">
-    <!-- Tlačítka pro zobrazení formulářů -->
-    <button class="button" type="button"  onclick="showForm('signin-form')">Sign in</button>
-    <button class="button" type="button"  onclick="showForm('login-form')">Login</button>
- 
-  </div>
   
   <!-- Formulář pro registraci (Sign in) -->
   <div id="signin-form" class="form-container" style="display: none;">
@@ -64,8 +45,6 @@ index.php
       </form>
   </div>
 
-</body>
-</html>
 
 </body>
 </html>
