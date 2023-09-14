795c85b4ecb4d6f3f7256422af2c94827385adcc
Olga Pecenkova <olga.pecenkova@gmail.com>
2023-09-14 21:48:46 +0200

    funkce pro smazání úkolu z databáze

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function showForm(formId) {
     // Zobrazíme vybraný formulář
   document.getElementById(formId).style.display = "block";
 }
+
+function deleteTask(taskId) {
+    if (confirm("Opravdu chcete smazat tento úkol?")) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          var response = JSON.parse(this.responseText);
+          if (response.success) {
+            // Odstranění řádku zobrazeného úkolu ze stránky
+            
+            var taskButton = document.querySelector("button[data-task-id='" + taskId + "']");
+            var taskRow = taskButton.closest("tr");
+            if (taskRow) {
+              taskRow.remove();
+            }
+          } else {
+            alert(response.message);
+          }
+        }
+      };
+      xhttp.open("POST", "delete_task.php", true);
+      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhttp.send("delete-task=" + taskId);
+    }
+  }
