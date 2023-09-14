584398986249e093ab3dd40bd05392d93df53840
Olga Pecenkova <olga.pecenkova@gmail.com>
2023-09-14 21:41:19 +0200

    vkládání nových úkolů do databáze

--- index_ukoly.php
+++ index_ukoly.php
@@ -28,6 +28,48 @@
     <button type="submit">Přidat&nbsp;úkol</button>
   </form>
 
+  <?php
+   require_once('config.php');
+  session_start();
+  // Zpracování formuláře po odeslání
+  if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["task-input"])) {
+    // Získání zadaného úkolu z formuláře
+    $task = $_POST["task-input"];
+    $taskpodr = $_POST["task-podrobnosti"];
+    $taskterm = $_POST["task-termin"];
+    $taskprior = $_POST["task-priorita"];
+
+  if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+  }
+    // Připojení k MySQL databázi
+    $conn =  mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    // Kontrola připojení
+    if (!$conn) {
+      die("Připojení k databázi selhalo: " . mysqli_connect_error());
+    }
+    if (!empty($username)) {
+
+      header("Location: index_úkoly.php");
+  } 
+    // Příprava dotazu pro vložení úkolu do tabulky
+    $sql = "INSERT INTO tasks (username, task_name, podrobnosti, termin, priorita) VALUES ('$username', '$task', '$taskpodr','$taskterm', '$taskprior')";
+
+    // Spuštění dotazu
+    if (mysqli_query($conn, $sql)) {
+      
+    } else {
+      echo '<script>
+      alert("Chyba při ukládání úkolu.");
+      window.history.back();
+      </script>';
+    }
+
+    // Uzavření spojení s databází
+    mysqli_close($conn);
+  }
+  ?>
 
   </body>
 </html>
