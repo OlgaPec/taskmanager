ad70174ed6e97b68d7f57652baf8a833e4fb4924
Olga Pecenkova <olga.pecenkova@gmail.com>
2023-09-14 20:05:00 +0200

    Hlavni nabídka taskmanager - signin -login

---
+++ index.php
@@ -0,0 +1,25 @@
+index.php
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Task manager</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+
+<h1>Task manager</h1>
+  
+  <div class="buttons-container">
+    <!-- Tlačítka pro zobrazení formulářů -->
+    <button class="button" type="button"  onclick="showForm('signin-form')">Sign in</button>
+    <button class="button" type="button"  onclick="showForm('login-form')">Login</button>
+ 
+  </div>
+  
+
+
+</body>
+</html>
