3cb093703f04f4c8de279b522bcadf74d80da46a
Olga Pecenkova <olga.pecenkova@gmail.com>
2023-09-14 21:07:04 +0200

    v souboru index_ukoly - tlačítko pro odhlášení

---
+++ index_ukoly.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Správa úkolů</title>
+  
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+
+<h1>Správa úkolů</h1>
+ 
+  <form action="logout.php" method="post">
+      <button type="submit">Odhlásit se</button>
+  </form>
+
+
+
+  </body>
+</html>
